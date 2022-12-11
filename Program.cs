0f324b2bd0d687f6b2d467f25058710065e3d8d2
Kirill <awerin134@gmail.com>
2022-12-11 02:17:09 -0800

    5

--- Program.cs
+++ Program.cs
@@ -1,16 +1,15 @@
-﻿int rows = int.Parse(Console.ReadLine()!);
+﻿System.Console.WriteLine("Введите длинну и ширину массива");
+int rows = int.Parse(Console.ReadLine()!);
 int[,] array = new int[rows,rows];
-int temp = 1;
 int i = 0;
 int j = 0;
-while(temp <= rows*rows)
+for(int temp =1; temp <= rows*rows; temp++)
 {
     array[i,j] = temp;
     if(i <= j + 1 && i + j < rows-1) j++;
     else if(i < j && i + j >= rows - 1) i++;
     else if(i >= j && i+ j > rows - 1) j--;
     else i--;
-    temp++;
 }
 for (int x = 0; x < array.GetLength(0); x++)
 {
