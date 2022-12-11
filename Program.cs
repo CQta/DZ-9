0516f5a03163f066e7f1380b8b02af5a97ad358e
Kirill <awerin134@gmail.com>
2022-12-11 04:45:52 -0800

    3

--- Program.cs
+++ Program.cs
@@ -1,21 +1,12 @@
-﻿System.Console.WriteLine("Введите длинну и ширину массива");
-int rows = int.Parse(Console.ReadLine()!);
-int[,] array = new int[rows,rows];
-int i = 0;
-int j = 0;
-for(int temp =1; temp <= rows*rows; temp++)
+﻿int Fun_Akkerman(int a, int b)
 {
-    array[i,j] = temp;
-    if(i <= j + 1 && i + j < rows-1) j++;
-    else if(i < j && i + j >= rows - 1) i++;
-    else if(i >= j && i+ j > rows - 1) j--;
-    else i--;
-}
-for (int x = 0; x < array.GetLength(0); x++)
-{
-    for (int y = 0; y < array.GetLength(1); y++)
-    {
-        System.Console.Write($"{array[x,y]}\t");
-    }
-    System.Console.WriteLine();
-}
+    if(a==0) return b+1;
+    if(a>0 && b==0) return Fun_Akkerman(a-1,1);
+    if(a>0 && b>0) return Fun_Akkerman(a-1, Fun_Akkerman(a,b-1)); 
+    return b+1;
+}   
+System.Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите 2 число");
+int num2 = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine(Fun_Akkerman(num, num2));
